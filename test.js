a2b5b20ed65f99a50b64220533e50937beef9120
aliao <aliao_12345@126.com>
2022-02-23 17:53:34 +0800

    '完成字符串匹配函数'

--- test.js
+++ test.js
@@ -13,9 +13,10 @@ const delSpace = function (str, pos = 'all') {
       pt: /\s+$/g
     },
     center: {
-      // 中间匹配正则不对
-      pt: /(\S+)(\s+)(\S+)/g,
-      grp: '$1$3'
+      // x(?!y)的意思是当x后面不是空格或者字符串结尾时
+      // 这个模式排除了字符串结尾的空格被匹配
+      pt: /(\S+)(\s+)(?!$|\s)/g,
+      grp: '$1'
     },
     both: {
       pt: /(^\s+)|(\s+$)/g
@@ -23,5 +24,5 @@ const delSpace = function (str, pos = 'all') {
   }
   return str.replace(patterns[pos].pt, patterns[pos].grp ? patterns[pos].grp : '')
 }
-// console.log('---', delSpace(' s f f f f f  ', 'center'), '----')
-console.log('---', delSpace(' s f f f f f  ', 'left'), '----')
+console.log('---', delSpace(' s f     f    f f f  ', 'center'), '----')
+// console.log('---', delSpace(' s f f f f f  ', 'left'), '----')
